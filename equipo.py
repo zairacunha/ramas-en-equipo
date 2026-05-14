85a4a0516dcbd929fa1cccedead0ed8de097af8f
Zaira Cunha <zairacunhadasilva@gmail.com>
2026-05-13 23:14:55 -0300

    Mejora título con emojis

--- equipo.py
+++ equipo.py
@@ -1,7 +1,7 @@
 # equipo.py - Proyecto en equipo
 
 def main():
-    print("=== Proyecto en Equipo ===")
+    print("=== 🚀 Super Proyecto en Equipo 🚀 ===")
     print("Bienvenidos a nuestro programa colaborativo")
 if __name__ == "__main__":
     main()

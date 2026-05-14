e606055447c1df942ee370628c3866503e64500c
Zaira Cunha <zairacunhadasilva@gmail.com>
2026-05-13 22:45:31 -0300

    Crear archivo base del proyecto

---
+++ equipo.py
@@ -0,0 +1,7 @@
+# equipo.py - Proyecto en equipo
+
+def main():
+    print("=== Proyecto en Equipo ===")
+    print("Bienvenidos a nuestro programa colaborativo")
+if __name__ == "__main__":
+    main()
